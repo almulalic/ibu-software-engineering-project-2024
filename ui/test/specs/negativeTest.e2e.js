2e001a873c00f589c619c11c0485562e925c150a
Almir Mulalić <47641522+almulalic@users.noreply.github.com>
2024-06-17 23:01:20 +0200

    E2E Test: Like/Unlike Feature (#34)
    * Issue 18: Admin Dashboard (#28)
    
    * added backend
    
    * added frontend
    
    * Add like-unlike test base
    
    * Issue 26: Like/Unlike Events Feature (#27)
    
    * Add backend changes for like/unlike feature
    
    * Add routes for events in focus and user liked events
    
    * Add like feature to event card
    
    * Add events in focus section
    
    * Add liked events section to my events page
    
    * Cleanup
    
    * E2E tests (#31)
    
    * Release 1 (#30)
    
    * Issue 18: Admin Dashboard (#28)
    
    * added backend
    
    * added frontend
    
    * Issue 26: Like/Unlike Events Feature (#27)
    
    * Add backend changes for like/unlike feature
    
    * Add routes for events in focus and user liked events
    
    * Add like feature to event card
    
    * Add events in focus section
    
    * Add liked events section to my events page
    
    * Issue 19: Edit Profile (#29)
    
    * Add routes and service methods to support edit profile
    
    * Remove password from edit request
    
    * Add JWT support on auth backend
    
    * Add UI for Edit Profile
    
    ---------
    
    Co-authored-by: arzeminaa <127847014+arzeminaa@users.noreply.github.com>
    Co-authored-by: Hanahu <92021975+Hanahu@users.noreply.github.com>
    
    * E2E tests
    
    * Added wdio package
    
    * Minor fix
    
    * Added negative tests
    
    ---------
    
    Co-authored-by: Almir Mulalić <47641522+almulalic@users.noreply.github.com>
    Co-authored-by: Hanahu <92021975+Hanahu@users.noreply.github.com>
    Co-authored-by: Arzemina - Fakultet <arzemina-fakultet@Arzeminas-MacBook-Pro-M1.local>
    
    * Add like-unlike test base
    
    * Fix user creation
    
    * Fix typo
    
    * Fix router
    
    * Add e2e test
    
    ---------
    
    Co-authored-by: arzeminaa <127847014+arzeminaa@users.noreply.github.com>
    Co-authored-by: hanahu <hanah.sijercic@stu.ibu.edu.ba>
    Co-authored-by: Hanahu <92021975+Hanahu@users.noreply.github.com>
    Co-authored-by: Arzemina - Fakultet <arzemina-fakultet@Arzeminas-MacBook-Pro-M1.local>

--- ui/test/specs/negativeTest.e2e.js
+++ ui/test/specs/negativeTest.e2e.js
@@ -1,53 +1,70 @@
-const { expect } = require('@wdio/globals')
-const LoginPage = require('../pageobjects/login.page')
-const SignupPage = require('../pageobjects/signup.page')
-const HomePage = require('../pageobjects/home.page')
+const LoginPage = require("../pageobjects/login.page");
+const SignupPage = require("../pageobjects/signup.page");
 
-let errorMessageInvalidEmailAddress = 'Please enter a valid email address!'
-let errorMessagePasswordTooShort = 'Password must be at least 6 characters!'
-let errorMessageAgreeWithTOC = 'Please agree with TOC!'
-let firstName = 'Arzemina'
-let lastName = 'Brackovic'
-let displayName = 'arzemina_b'
-let email = 'arzemina.brackovic@stu.ibu.edu.ba'
-let correctPassword = 'password8!'
-let incorrectPassword = '123'
+let errorMessageInvalidEmailAddress = "Please enter a valid email address!";
+let errorMessagePasswordTooShort = "Password must be at least 6 characters!";
+let errorMessageAgreeWithTOC = "Please agree with TOC!";
+let firstName = "Arzemina";
+let lastName = "Brackovic";
+let displayName = "arzemina_b";
+let email = "arzemina.brackovic@stu.ibu.edu.ba";
+let correctPassword = "password8!";
+let incorrectPassword = "123";
 
-describe('Eventport application', () => {
-    it("Verify user can't sign up with same credentials", async () => {
-        await SignupPage.open()
+describe("Eventport application", () => {
+	it("Verify user can't sign up with same credentials", async () => {
+		await SignupPage.open();
 
-        await SignupPage.signUpWithSameCredentials(firstName, lastName, displayName, email, correctPassword)
-    })
+		await SignupPage.signUpWithSameCredentials(firstName, lastName, displayName, email, correctPassword);
+	});
 
-    it("Verify user can't sign up with invalid email address", async () => {
-        await SignupPage.open()
+	it("Verify user can't sign up with invalid email address", async () => {
+		await SignupPage.open();
 
-        await SignupPage.signUpWithInvalidEmailAddress(firstName, lastName, displayName, email, errorMessageInvalidEmailAddress)
-    })
+		await SignupPage.signUpWithInvalidEmailAddress(
+			firstName,
+			lastName,
+			displayName,
+			email,
+			errorMessageInvalidEmailAddress
+		);
+	});
 
-    it("Verify user can't sign up with password that is isn't at least 6 characters long", async () => {
-        await SignupPage.open()
+	it("Verify user can't sign up with password that is isn't at least 6 characters long", async () => {
+		await SignupPage.open();
 
-        await SignupPage.signUpWithShortPassword(firstName, lastName, displayName, email, incorrectPassword, errorMessagePasswordTooShort)
-    })
+		await SignupPage.signUpWithShortPassword(
+			firstName,
+			lastName,
+			displayName,
+			email,
+			incorrectPassword,
+			errorMessagePasswordTooShort
+		);
+	});
 
-    it("Verify user can't sign up without agreeing with TOC", async () => {
-        await SignupPage.open()
+	it("Verify user can't sign up without agreeing with TOC", async () => {
+		await SignupPage.open();
 
-        await SignupPage.signUpWithoutCheckingTOC(firstName, lastName, displayName, email, correctPassword, errorMessageAgreeWithTOC)
-    })
+		await SignupPage.signUpWithoutCheckingTOC(
+			firstName,
+			lastName,
+			displayName,
+			email,
+			correctPassword,
+			errorMessageAgreeWithTOC
+		);
+	});
 
-    it("Verify user can't login with invalid email address", async () => {
-        await LoginPage.open()
+	it("Verify user can't login with invalid email address", async () => {
+		await LoginPage.open();
 
-        await LoginPage.loginWithInvalidEmailAddress(firstName, errorMessageInvalidEmailAddress)
-    })
+		await LoginPage.loginWithInvalidEmailAddress(firstName, errorMessageInvalidEmailAddress);
+	});
 
-    it("Verify user can't login with password that isn't at least 6 characters long", async () => {
-        await LoginPage.open()
+	it("Verify user can't login with password that isn't at least 6 characters long", async () => {
+		await LoginPage.open();
 
-        await LoginPage.loginWithShortPassword(email, incorrectPassword, errorMessagePasswordTooShort)
-    })
-
-})
+		await LoginPage.loginWithShortPassword(email, incorrectPassword, errorMessagePasswordTooShort);
+	});
+});
